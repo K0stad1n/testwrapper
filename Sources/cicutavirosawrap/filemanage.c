663dbbec814d7a98d48f75a8ff6f5f2624d796d8
K0stad1n <razer.saiso@gmail.com>
2021-02-15 20:05:57 +0100

    added more permissions for filemanage

--- Sources/cicutavirosawrap/filemanage.c
+++ Sources/cicutavirosawrap/filemanage.c
@@ -9,12 +9,12 @@
 #include "include/File.h"
 void initlogfile(){
     remove("log.txt");
-    FILE *f = fopen("log.txt", "w");
+    FILE *f = fopen("log.txt", "w+");
     fclose(f);
 }
 
 void writetofile(char* msg){
-    FILE *f = fopen("log.txt", "a");
+    FILE *f = fopen("log.txt", "a+");
     fprintf(f, "%s", msg);
     fclose(f);
 }
