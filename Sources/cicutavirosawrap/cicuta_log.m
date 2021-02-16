2d2a0e8d16f3790c169e3c36dea0e47748dc87c0
Sangster <61627524+Sangster-5@users.noreply.github.com>
2021-02-16 16:11:25 -0400

    Update and rename cicuta_log.c to cicuta_log.m

--- Sources/cicutavirosawrap/cicuta_log.m
+++ Sources/cicutavirosawrap/cicuta_log.m
@@ -10,6 +10,7 @@ void cicuta_log(const char* format, ...)
     va_start(ap, format);
     vasprintf(&msg, format, ap);
     printf("%s\n", msg);
+    NSString *message = [NSString stringWithFormat:@"%s", msg];
     va_end(ap);
     free(msg);
 }
